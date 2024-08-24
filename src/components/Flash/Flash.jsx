b0d2256e13e6c173e90279c9fb42b3da556b98c1
Conrado julio <conradojuliosisnero@gmail.com>
2024-08-24 16:31:54 -0500

    efecto de sol en la parte superior

---
+++ src/components/Flash/Flash.jsx
@@ -0,0 +1,12 @@
+import "@/styles/globals.css";
+
+const Flash = () => {
+  return (
+    <div style={{ position: "relative" }}>
+      <div class="sun"></div>
+      <div class="ray"></div>
+    </div>
+  );
+};
+
+export default Flash;
